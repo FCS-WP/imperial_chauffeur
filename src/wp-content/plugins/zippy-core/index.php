7c0968359c7737707184808d199c5aa214cee742
shin <nhathanh024@gmail.com>
2025-06-03 11:03:16 +0700

    Update SMTP server

--- src/wp-content/plugins/zippy-core/index.php
+++ src/wp-content/plugins/zippy-core/index.php
@@ -1,2 +1,7 @@
-<?php
-# Silence is golden.
+<div class="social-icons">
+  <a class="facebook" href="https://www.facebook.com/epossg"><img src="/wp-content/uploads/2025/05/bxl-facebook.svg" alt="Facebook" /></a>
+  <a href="https://www.instagram.com/epossg/"><img src="/wp-content/uploads/2025/05/instagram-logo.svg" alt="Instagram" /></a>
+  <a href="https://www.youtube.com/@epospossystemsingapore9807"><img src="/wp-content/uploads/2025/05/youtube-logo.svg" alt="YouTube" /></a>
+  <a href="https://www.linkedin.com/company/14524422"><img class="icon-linkedin" src="/wp-content/uploads/2025/05/linkedin.png" alt="LinkedIn" /></a>
+  <a href="https://www.xiaohongshu.com/user/profile/5ecf5cbc000000000100162e"><img class="icon-xiaohongshu" src="/wp-content/uploads/2025/05/xiaohongshu.png" alt="Website" /></a>
+</div>
