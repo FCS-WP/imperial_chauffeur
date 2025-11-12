2ea7c8388f2ce9d2f7dc5196f64df4261f8f6dde
thonghm <thong.haminh@floatingcube.com>
2025-11-12 14:25:33 +0700

    update description product

--- src/wp-content/themes/zippy-elementor-child/includes/zippy-woo.php
+++ src/wp-content/themes/zippy-elementor-child/includes/zippy-woo.php
@@ -281,3 +281,62 @@ function add_customer_v2_role()
   }
 }
 add_action('init', 'add_customer_v2_role');
+
+
+add_shortcode('descrition_product', 'render_descrition_product_shortcode');
+function render_descrition_product_shortcode()
+{
+  global $product;
+
+  if (!$product) {
+    return '';
+  }
+
+  $product_id = $product->get_id();
+  $is_role_customer_v2 = false;
+  if (is_user_logged_in()) {
+    $user = wp_get_current_user();
+    if ($user->ID) {
+      if (in_array('customer_v2', (array) $user->roles)) {
+        $is_role_customer_v2 = true;
+      }
+    }
+  }
+
+  $discounted_price = get_product_pricing_rules($product, 1);
+  $regular_price = !empty($discounted_price) ? $discounted_price : $product->get_price();
+
+  $price_per_hour_for_v2 = get_config_price_for_customer_v2();
+  if ($is_role_customer_v2 && array_key_exists($product_id, $price_per_hour_for_v2)) {
+    $price_per_hour = $price_per_hour_for_v2[$product_id];
+  } else {
+    $price_per_hour = get_post_meta($product_id, '_price_per_hour', true);
+    $price_per_hour = (!empty($price_per_hour) && is_numeric($price_per_hour)) ? (float) $price_per_hour : $regular_price;
+  }
+
+  return '<table class="description-product-table">
+            <tr>
+              <td><strong>Transfer</strong></td>
+              <td>SGD ' . $regular_price . ' / Trip</td>
+            </tr>
+            <tr>
+              <td><strong>Disposal (Min. 3 Hours)</strong></td>
+              <td>SGD ' . $price_per_hour . ' / Hour</td>
+            </tr>
+          </table>';
+}
+
+
+function get_config_price_for_customer_v2()
+{
+  return [
+    '48' => 180,
+    '46' => 130,
+    '44' => 55,
+    '52' => 55,
+    '50' => 55,
+    '54' => 55,
+    '38' => 75,
+    '40' => 90,
+  ];
+}
